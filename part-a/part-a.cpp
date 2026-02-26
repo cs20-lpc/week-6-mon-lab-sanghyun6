cbe916a554c09c17632a2ebc43b069e993c135d1
sanghyun6 <sanghyunleeee@gmail.com>
2026-02-25 16:40:29 -0800

    week6lab

--- part-a/part-a.cpp
+++ part-a/part-a.cpp
@@ -7,55 +7,91 @@ using namespace std;
 
 /*******************************************************************************
  * Function prototype
-*******************************************************************************/
-
+ *******************************************************************************/
+// time complexity is O(n) since it visits the whole array one time
 template <typename T>
-T findMaxRecTail(const T[] arr, const int size, int = 0)
+T findMaxHelper(const T arr[], const int size, T mx, int i)
 {
-    // TO DO: Implement your code
+    if (i == size)
+    {
+        return mx;
+    }
+
+    if (arr[i] > mx)
+    {
+        mx = arr[i];
+    }
+    return findMaxHelper(arr, size, mx, i + 1);
 }
 
 template <typename T>
-T findMaxRecBinarySplit(const T[] arr, const int left, const int right)
+T findMaxRecTail(const T arr[], const int size, int i = 0)
 {
     // TO DO: Implement your code
+    return findMaxHelper(arr, size, arr[0], i + 1);
 }
+
+// time complexity is O(n) since it visits the whole array one time
+template <typename T>
+T findMaxRecBinarySplit(const T arr[], const int left, const int right)
+{
+    // TO DO: Implement your code
+    if (left == right)
+    {
+        return arr[left];
+    }
+    int mid = (right + left) / 2;
+    T leftmx = findMaxRecBinarySplit(arr, left, mid);
+    T rightmx = findMaxRecBinarySplit(arr, mid + 1, right);
+
+    if (leftmx > rightmx)
+    {
+        return leftmx;
+    }
+    else
+    {
+        return rightmx;
+    }
+}
+
 /*******************************************************************************
  * Description:
  * Starting point of the program. Creates two arrays, one fixed and the other
  * random. Determines the maximum value by calling the local function and the
  * standard function.
- * 
+ *
  * Input:
  * N/A
  *
  * Output:
  * An integer to signal to the OS the exit code.
-*******************************************************************************/
+ *******************************************************************************/
 
-int main() {
+int main()
+{
     // create the array
     const int SIZE = 10;
     int myArray[SIZE] = {
         5, 23, 0, -2, 4,
-        9, 11, 21, 130, 6
-    };
+        9, 11, 21, 130, 6};
 
     // display the maximum
     cout << setfill('-') << setw(40) << "" << endl;
     cout << "Maximum using Recursion: "
-         << findMaxRecTail(myArray, SIZE) << endl 
-         << "From Binary split:" 
-        << findMaxRecBinarySplit(myArray, 0, SIZE-1)
+         << findMaxRecTail(myArray, SIZE) << endl
+         << "From Binary split:"
+         << findMaxRecBinarySplit(myArray, 0, SIZE - 1)
          << "Should be 130 for the fixed array\n";
-    cout << setfill('-') << setw(40) << "" << endl << endl;
+    cout << setfill('-') << setw(40) << "" << endl
+         << endl;
 
     // create a random array
     const int SIZE_2 = 1000;
     const int MAX_VAL = 10000;
     int randArray[SIZE_2];
     srand(time(0));
-    for (int& elem : randArray) {
+    for (int &elem : randArray)
+    {
         elem = rand() % MAX_VAL;
     }
 
@@ -72,4 +108,3 @@ int main() {
     // terminate
     return 0;
 }
-
--- part-b/part-b.cpp
+++ part-b/part-b.cpp
@@ -5,29 +5,50 @@ using namespace std;
 
 /*******************************************************************************
  * Function prototype
-*******************************************************************************/
+ *******************************************************************************/
 
-void towerHanoi(int, string, string, string, int&);
+// time complexity is O(2^n) since one more call generates two more recursive call
+void towerHanoi(int num, string source, string buffer, string target, int &numMoves)
+{
+    if (num <= 0)
+    {
+        return;
+    }
+
+    // 2. n-1개의 원반을 Source에서 Buffer로 옮긴다 (Target을 보조 기둥으로 활용)
+    towerHanoi(num - 1, source, target, buffer, numMoves);
+
+    // 3. 가장 밑에 있는(가장 큰) 원반을 Target으로 옮긴다
+    numMoves++; // 이동 횟수 기록
+    cout << "Moving disc " << num << " from " << source << " to " << target << endl;
+
+    // 4. Buffer에 치워뒀던 n-1개의 원반을 다시 Target으로 옮긴다 (Source를 보조 기둥으로 활용)
+    towerHanoi(num - 1, buffer, source, target, numMoves);
+}
 
 /*******************************************************************************
  * Description:
  * Starting point of the program. Calls a function to recursively determine
  * solutions for the first 5 instances of the Tower of Hanoi problem.
- * 
+ *
  * Input:
  * N/A
  *
  * Output:
  * An integer to signal to the OS the exit code.
-*******************************************************************************/
+ *******************************************************************************/
 
-int main() {
+int main()
+{
     // solve the first 5 cases of the Tower of Hanoi problem
-    for (int i = 1, numMoves = 0; i <= 5; i++, numMoves = 0) {
+    for (int i = 1, numMoves = 0; i <= 5; i++, numMoves = 0)
+    {
         cout << setfill('-') << setw(40) << "" << endl;
-        cout << "Trying n = " << i << endl << endl;
+        cout << "Trying n = " << i << endl
+             << endl;
         towerHanoi(i, "Rod #1", "Rod #2", "Rod #3", numMoves);
-        cout << endl << "Number of Moves = " << numMoves << endl;
+        cout << endl
+             << "Number of Moves = " << numMoves << endl;
     }
 
     // terminate
